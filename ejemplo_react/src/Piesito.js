1fe13d009d4218227b9c069add3b785d54f71e1e
enti <enti@enti.enti>
2022-02-25 14:29:17 +0100

    JSX(Titulito y Piesito)

---
+++ ejemplo_react/src/Piesito.js
@@ -0,0 +1,15 @@
+import './Piesito.css';
+
+function Piesito () 
+{
+
+return(
+	<footer>
+	<p>Copy(c)2022 La Mamá de Piesito</p>
+	</footer>
+	);
+
+}
+
+
+export default Piesito;
---
+++ ejemplo_react/src/Titulito.js
@@ -0,0 +1,14 @@
+
+function Titulito ()
+{
+	return(
+		<div id="titulo">
+		<h1>ola k ase</h1>
+		<p><strong><em>El mejor título del mundo</em></strong></p>
+		</div>
+	);
+
+}
+
+
+export default Titulito;
